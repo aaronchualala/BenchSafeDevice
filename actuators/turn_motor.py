00a66e9b273eda36e525ea3d5638617d2d247488
Shawn Chan <shawnchan@Shawns-MacBook-Pro.local>
2023-03-03 19:13:42 +0800

    updated turn motor py

--- actuators/turn_motor.py
+++ actuators/turn_motor.py
@@ -2,7 +2,10 @@ import RPi.GPIO as GPIO
 from RpiMotorLib import RpiMotorLib
 
 def turn_motor(angle):
-    GpioPins = [18, 23, 24, 25]
+    GpioPins = [17, 18, 27, 22]
     mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "28BYJ")
     mymotortest.motor_run(GpioPins , .01, 100, False, False, "half", .05)
     return
+
+while True:
+    turn_motor(180)
