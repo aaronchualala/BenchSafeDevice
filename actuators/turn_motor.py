884e98d1263168914337792d2cfbacc59e302c48
Aaron Chua <81175540+aaronchualala@users.noreply.github.com>
2023-03-03 19:47:34 +0800

    Update turn_motor.py

--- actuators/turn_motor.py
+++ actuators/turn_motor.py
@@ -6,6 +6,6 @@ def turn_motor(angle):
     GpioPins = [18, 23, 24, 25]
     mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "28BYJ")
     time.sleep(0.1)
-    mymotortest.motor_run(GpioPins,.05,128, True, True,"full", .05)
+    mymotortest.motor_run(GpioPins,.001,256, False, True,"half", .05)
     GPIO.cleanup()
     return
