d392c98e9abfc65bcdf16e175bce66e4c716e137
Aaron Chua <81175540+aaronchualala@users.noreply.github.com>
2023-03-17 22:40:30 +0800

    edit turn_motor

--- actuators/turn_motor.py
+++ actuators/turn_motor.py
@@ -7,7 +7,6 @@ def turn_motor(steps):
     # GpioPins = [18, 23, 24, 25]
     # mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "28BYJ")
     # mymotortest.motor_run(GpioPins , .01, 100, False, True, "half", .05)
-    GPIO.setmode(GPIO.BOARD)
     print('steps', steps)
     direction = True if steps > 0 else False
     GpioPins = [18, 23, 24, 25]
