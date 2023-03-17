e07151cddb47df4d5da4fff462b8ceb39835d523
Shawn Chan <shawnchan@Shawns-MacBook-Pro.local>
2023-03-17 22:26:29 +0800

    change calculate_bench_distance

--- ultrasonic/calculate_bench_distance.py
+++ ultrasonic/calculate_bench_distance.py
@@ -4,9 +4,10 @@ import time
 ultrasonic = DistanceSensor(echo=17, trigger=4, max_distance = 300)
 
 def calculate_bench_distance():
-    count = 100
+    count = 30
     while count != 0:
-        print(f"{ultrasonic.distance*300} cm")
+        dist = ultrasonic.distance*300
+        print(f"{dist}cm")
         time.sleep(1)
         count -=1
     
