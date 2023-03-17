775f2ef43433f4743aaad91dcc1ab9fc4edcff19
Aaron Chua <81175540+aaronchualala@users.noreply.github.com>
2023-03-17 22:52:10 +0800

    edit turn_motor

--- actuators/turn_motor.py
+++ actuators/turn_motor.py
@@ -3,14 +3,14 @@ import RPi.GPIO as GPIO
 from RpiMotorLib import RpiMotorLib
 
 
-def turn_motor(steps):
+def turn_motor(number_of_steps, is_ccwise):
     my_motor = RpiMotorLib.BYJMotor("MyMotor", "28BYJ")
     
     my_motor.motor_run(
         gpiopins=[18, 23, 24, 25], 
         wait=.001, 
-        steps=abs(int(steps)), 
-        ccwise=(True if steps > 0 else False), 
+        steps=number_of_steps, 
+        ccwise=is_ccwise, 
         verbose=False, 
         steptype="half", 
         initdelay=0.05)
