a97e207fe0be61b5b3591529583b514f2be4f12d
Aaron Chua <81175540+aaronchualala@users.noreply.github.com>
2023-03-17 23:30:03 +0800

    update bench_dist

--- ultrasonic/calculate_bench_distance.py
+++ ultrasonic/calculate_bench_distance.py
@@ -15,29 +15,29 @@ import RPi.GPIO as GPIO
 
 def calculate_bench_distance(echo=17, trigger=4):
     # GPIO.setmode(GPIO.BOARD)
-    pinTrigger = trigger
-    pinEcho = echo
-    GPIO.setup(pinTrigger, GPIO.OUT)
-    GPIO.setup(pinEcho, GPIO.IN)
+    # pinTrigger = trigger
+    # pinEcho = echo
+    # GPIO.setup(pinTrigger, GPIO.OUT)
+    # GPIO.setup(pinEcho, GPIO.IN)
 
-    GPIO.output(pinTrigger, GPIO.LOW)
-    GPIO.output(pinTrigger, GPIO.HIGH)
+    # GPIO.output(pinTrigger, GPIO.LOW)
+    # GPIO.output(pinTrigger, GPIO.HIGH)
 
-    time.sleep(0.00001)
-    GPIO.output(pinTrigger, GPIO.LOW)
+    # time.sleep(0.00001)
+    # GPIO.output(pinTrigger, GPIO.LOW)
 
-    count = 30
-    while count != 0:
-        while GPIO.input(pinEcho)==0:
-            print("GPIO.input(pinEcho)==0")
-            pulseStartTime = time.time()
-        while GPIO.input(pinEcho)==1:
-            print("GPIO.input(pinEcho)==1")
-            pulseEndTime = time.time()
+    # count = 30
+    # while count != 0:
+    #     while GPIO.input(pinEcho)==0:
+    #         print("GPIO.input(pinEcho)==0")
+    #         pulseStartTime = time.time()
+    #     while GPIO.input(pinEcho)==1:
+    #         print("GPIO.input(pinEcho)==1")
+    #         pulseEndTime = time.time()
         
-        pulseDuration = pulseEndTime - pulseStartTime
-        distance = round(pulseDuration*17150, 2)
-        print("distance: " + str(distance) + " cm")
-        count -= 1
+    #     pulseDuration = pulseEndTime - pulseStartTime
+    #     distance = round(pulseDuration*17150, 2)
+    #     print("distance: " + str(distance) + " cm")
+    #     count -= 1
     
-    return distance
+    return 2
