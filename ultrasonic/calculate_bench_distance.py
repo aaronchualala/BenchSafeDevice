f0a2610d6a59ae64d49c4f818008950b6be8b06d
Shawn Chan <shawnchan@Shawns-MacBook-Pro.local>
2023-03-17 22:16:43 +0800

    updated calculate_bench_distance

--- ultrasonic/calculate_bench_distance.py
+++ ultrasonic/calculate_bench_distance.py
@@ -1,9 +1,9 @@
 from click import echo
 from gpiozero import DistanceSensor
 import time
-ultrasonic = DistanceSensor(echo=17, trigger=4)
+ultrasonic = DistanceSensor(echo=17, trigger=4, max_distance = 300)
 
 def calculate_bench_distance():
     while True:
-        print(ultrasonic.distance)
+        print(f"{ultrasonic.distance*300} cm")
         time.sleep(1)
