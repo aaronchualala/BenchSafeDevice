cdc5432c37c92a74b7d3f570607ac77f5c839521
Aaron Chua <81175540+aaronchualala@users.noreply.github.com>
2023-03-17 22:44:57 +0800

    edit turn_motor

--- actuators/turn_motor.py
+++ actuators/turn_motor.py
@@ -4,13 +4,6 @@ from RpiMotorLib import RpiMotorLib
 
 
 def turn_motor(steps):
-    # GpioPins = [18, 23, 24, 25]
-    # mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "28BYJ")
-    # mymotortest.motor_run(GpioPins , .01, 100, False, True, "half", .05)
-    print('steps', steps)
-    print('steps', int(steps))
-    print('steps', abs(int(steps)))
-
     direction = True if steps > 0 else False
     GpioPins = [18, 23, 24, 25]
     my_motor = RpiMotorLib.BYJMotor("MyMotor", "28BYJ")
@@ -24,5 +17,5 @@ def turn_motor(steps):
         steptype="half", 
         initdelay=0.05)
     print("HELP")
-    GPIO.cleanup()
+    # GPIO.cleanup()
     return
