b12a4346b49169c08665b095e3544ea5ba3de981
Aaron Chua <81175540+aaronchualala@users.noreply.github.com>
2023-03-17 22:22:08 +0800

    revert turn_motor

--- actuators/turn_motor.py
+++ actuators/turn_motor.py
@@ -3,21 +3,24 @@ import RPi.GPIO as GPIO
 from RpiMotorLib import RpiMotorLib
 
 def turn_motor(steps):
-    direction = True if steps > 0 else False
     GpioPins = [18, 23, 24, 25]
-    my_motor = RpiMotorLib.BYJMotor("MyMotor", "28BYJ")
-    time.sleep(0.1)
+    mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "28BYJ")
+    mymotortest.motor_run(GpioPins , .01, 100, False, False, "half", .05)
+    # direction = True if steps > 0 else False
+    # GpioPins = [18, 23, 24, 25]
+    # my_motor = RpiMotorLib.BYJMotor("MyMotor", "28BYJ")
+    # time.sleep(0.1)
 
-    print("HELP")
-    my_motor.motor_run(
-        gpiopins=GpioPins, 
-        wait=.001, 
-        steps=512, #abs(steps), 
-        ccwise=False, # direction, 
-        verbose=True, 
-        steptype="half", 
-        initdelay=0.05)
-    print("MORE HELP")
-    GPIO.cleanup()
-    print("MOREEE HELP")
+    # print("HELP")
+    # my_motor.motor_run(
+    #     gpiopins=GpioPins, 
+    #     wait=.001, 
+    #     steps=512, #abs(steps), 
+    #     ccwise=False, # direction, 
+    #     verbose=True, 
+    #     steptype="half", 
+    #     initdelay=0.05)
+    # print("MORE HELP")
+    # GPIO.cleanup()
+    # print("MOREEE HELP")
     return
