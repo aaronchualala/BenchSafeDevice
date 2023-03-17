0fd171f89fd79e14685cb429cbd9a34136672a6f
Aaron Chua <81175540+aaronchualala@users.noreply.github.com>
2023-03-17 22:37:08 +0800

    edit calc

--- actuators/turn_motor.py
+++ actuators/turn_motor.py
@@ -3,24 +3,25 @@ import RPi.GPIO as GPIO
 from RpiMotorLib import RpiMotorLib
 
 def turn_motor(steps):
-    GpioPins = [18, 23, 24, 25]
-    mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "28BYJ")
-    mymotortest.motor_run(GpioPins , .01, 100, False, True, "half", .05)
-    # direction = True if steps > 0 else False
     # GpioPins = [18, 23, 24, 25]
-    # my_motor = RpiMotorLib.BYJMotor("MyMotor", "28BYJ")
-    # time.sleep(0.1)
+    # mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "28BYJ")
+    # mymotortest.motor_run(GpioPins , .01, 100, False, True, "half", .05)
+    print('steps', steps)
+    direction = True if steps > 0 else False
+    GpioPins = [18, 23, 24, 25]
+    my_motor = RpiMotorLib.BYJMotor("MyMotor", "28BYJ")
+    time.sleep(0.1)
 
-    # print("HELP")
-    # my_motor.motor_run(
-    #     gpiopins=GpioPins, 
-    #     wait=.001, 
-    #     steps=512, #abs(steps), 
-    #     ccwise=False, # direction, 
-    #     verbose=True, 
-    #     steptype="half", 
-    #     initdelay=0.05)
-    # print("MORE HELP")
-    # GPIO.cleanup()
-    # print("MOREEE HELP")
+    print("HELP")
+    my_motor.motor_run(
+        gpiopins=GpioPins, 
+        wait=.001, 
+        steps=512, #abs(steps), 
+        ccwise=False, # direction, 
+        verbose=True, 
+        steptype="half", 
+        initdelay=0.05)
+    print("MORE HELP")
+    GPIO.cleanup()
+    print("MOREEE HELP")
     return
