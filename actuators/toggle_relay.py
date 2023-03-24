d4cf892d61f612b4eb6cdb0ec7dc637e1295be6d
Aaron Chua <81175540+aaronchualala@users.noreply.github.com>
2023-03-24 23:25:11 +0800

    test

--- actuators/toggle_relay.py
+++ actuators/toggle_relay.py
@@ -4,18 +4,18 @@ import RPi.GPIO as GPIO
 
 def toggle_relay(state):
     gpio_pin = 21
-    dur = 1
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(gpio_pin, GPIO.OUT)
 
-    if state == "on":
+    while True:
+    # if state == "on":
         GPIO.output(gpio_pin, GPIO.HIGH)
         print("Relay On")
-    elif state == "off":
+    # elif state == "off":
         GPIO.output(gpio_pin, GPIO.LOW)
         print("Relay Off")
-    else:
-        print("Invalid argument. Use 'on' or 'off'")
+    # else:
+    #     print("Invalid argument. Use 'on' or 'off'")
 
     GPIO.cleanup()
     return
