13a42289db7b601ee00a9a5c603a7a2736955391
Aaron Chua <81175540+aaronchualala@users.noreply.github.com>
2023-03-24 20:23:39 +0800

    update

--- actuators/relay.py
+++ actuators/relay.py
@@ -3,9 +3,11 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(2, GPIO.OUT)
+
+GPIO.output(2, GPIO.LOW)
+time.sleep(2)
+GPIO.output(2, GPIO.HIGH)
+time.sleep(2)
 GPIO.output(2, GPIO.LOW)
 
-time.sleep(0.25)
-
-GPIO.output(2, GPIO.HIGH)
 GPIO.cleanup()
