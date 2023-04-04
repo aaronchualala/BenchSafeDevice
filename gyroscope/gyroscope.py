3be878b617bee7b6c04bca5b76b6a14e429da0c6
Aaron Chua <81175540+aaronchualala@users.noreply.github.com>
2023-04-04 22:42:32 +0800

    update gyro

--- gyroscope/gyroscope.py
+++ gyroscope/gyroscope.py
@@ -5,5 +5,9 @@ sensor = mpu6050(0x68)
 
 def get_angle_between_flat_bench_and_inclined_bench():
     accelerometer_data = sensor.get_accel_data()
-    print(accelerometer_data)
+    gyro_data = sensor.get_gyro_data()
+    temp = sensor.get_temp()
+    print("accelerometer_data: ", accelerometer_data)
+    print("gyro_data: ", gyro_data)
+    print("temp: ", temp)
     return 30
