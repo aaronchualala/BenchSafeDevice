73646513522f718f3057e18ca538de1ae1c85809
Aaron Chua <81175540+aaronchualala@users.noreply.github.com>
2023-04-04 22:47:41 +0800

    update gyro

--- gyroscope/gyroscope.py
+++ gyroscope/gyroscope.py
@@ -1,13 +1,14 @@
 # https://pypi.org/project/mpu6050-raspberrypi/
+# https://www.electronicwings.com/raspberry-pi/mpu6050-accelerometergyroscope-interfacing-with-raspberry-pi#:~:text=The%20MPU6050%20sensor%20module%20is,using%20its%20Auxiliary%20I2C%20bus
 from mpu6050 import mpu6050
 
 sensor = mpu6050(0x68)
 
 def get_angle_between_flat_bench_and_inclined_bench():
     accelerometer_data = sensor.get_accel_data()
-    gyro_data = sensor.get_gyro_data()
-    temp = sensor.get_temp()
-    print("accelerometer_data: ", accelerometer_data)
+    # gyro_data = sensor.get_gyro_data()
+    # temp = sensor.get_temp()
+    # print("accelerometer_data: ", accelerometer_data)
     print("gyro_data: ", gyro_data)
     print("temp: ", temp)
     return 30
