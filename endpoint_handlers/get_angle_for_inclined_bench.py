9cd8496095d9ea0319aa4ef7c98eb5e0d52132ce
Aaron Chua <81175540+aaronchualala@users.noreply.github.com>
2023-04-05 16:39:17 +0800

    edit calc

--- endpoint_handlers/get_angle_for_inclined_bench.py
+++ endpoint_handlers/get_angle_for_inclined_bench.py
@@ -22,6 +22,7 @@ def get_angle_for_inclined_bench(
     H = float(vertical_distance_from_flat_bench_to_device)
 
     l = B*math.cos(y) -  N*math.cos(y) - math.tan(theta)*(H - N*math.sin(y))
+    print("before l: ", B*math.cos(y) -  N*math.cos(y))
     print("l: ", l)
     angle = math.atan((H - B* math.sin(y))/(x+l)) * 180 / math.pi
 
